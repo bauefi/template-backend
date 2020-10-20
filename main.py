54cd78c0a1629854b9735428ac447b320ef3c9cc
Finn Bauer <finn.bauer18@imperial.ac.uk>
2020-10-20 22:58:19 +0200

    Add route for creating session for stripe checkout

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-import datetime
+import os
+import stripe
+from flask_cors import CORS, cross_origin
 
-from flask import Flask, render_template
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/hello')
-def hello():
-    return "Hello World"
+# Set your secret key. Remember to switch to your live secret key in production!
+# See your keys here: https://dashboard.stripe.com/account/apikeys
+stripe.api_key = 'sk_test_51HeN8eH4w9X1EWMYc1gRVyPgI7Dm9j6wjAyEKPJMzBohyIk2Yu3v8NVpFUYAxw595JioVs8uf15rdu5F87Dqstna00HmgQhF30'
+
+@app.route('/create-checkout-session', methods=['POST'])
+@cross_origin()
+def create_checkout_session():
+  session = stripe.checkout.Session.create(
+    payment_method_types=['card'],
+    line_items=[{
+      # Replace `price_...` with the actual price ID for your subscription
+      # you created in step 2 of this guide.
+      'price': 'price_1HeNI8H4w9X1EWMYAR1N08gE',
+      'quantity': 1,
+    }],
+    mode='subscription',
+    success_url='http://localhost:3000/success',
+    cancel_url='http://localhost:3000/',
+  )
+  print(session)
+  return jsonify(id=session.id)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
